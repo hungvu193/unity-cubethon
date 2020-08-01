e74c336d244a936ac3982d1f91ed94aa5d23b90a
hungvu <visaocuoitroi193@gmail.com>
2020-08-02 01:22:59 +0700

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+   private bool isGameEnded = false;
+   public GameObject completeLevelUI;
+
+   public void EndGame()
+    {
+        if (isGameEnded == false)
+        {
+            isGameEnded = true;
+            Debug.Log("Game Over");
+            Invoke("RestartGame", 2f);
+        }
+    }
+
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Level Won");
+        completeLevelUI.SetActive(true);
+    }
+    
+}
---
+++ Assets/Scripts/PlayCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (collision.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
